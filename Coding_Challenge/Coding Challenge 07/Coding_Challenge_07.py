d44e3dc4079729abb1a84a1a84ce6a1d1622d664
Andy Davies <andy.marinebio@googlemail.com>
2023-04-21 11:57:41 -0400

    Added Andy feedback

--- Coding_Challenge/Coding Challenge 07/Coding_Challenge_07.py
+++ Coding_Challenge/Coding Challenge 07/Coding_Challenge_07.py
@@ -1,8 +1,8 @@
-# Coding Challenge 7
+# Coding Challenge 7.py
 # Our coding challenge this week should make use of temporary folders, output folders and file management.
 #
-# Convert your Coding Challenge 5 exercise to work with temporary folders, os.path.join and glob.glob.
-# Do not take too much time on this and if you do not have a working Challenge 5, talk to the instructor.
+# Convert your Coding Challenge 5.py exercise to work with temporary folders, os.path.join and glob.glob.
+# Do not take too much time on this and if you do not have a working Challenge 5.py, talk to the instructor.
 
 import os
 import arcpy
@@ -108,70 +108,70 @@ arcpy.Delete_management(os.path.join(input_directory, "temporary_files"))
 
 
 
-# Describe the extent of the files
-desc = arcpy.Describe(saved_Layer)
-XMin = desc.extent.XMin
-XMax = desc.extent.XMax
-YMin = desc.extent.YMin
-YMax = desc.extent.YMax
+    # Describe the extent of the files
+    desc = arcpy.Describe(saved_Layer)
+    XMin = desc.extent.XMin
+    XMax = desc.extent.XMax
+    YMin = desc.extent.YMin
+    YMax = desc.extent.YMax
 
-# Set the spatial reference of the data (WGS 1984)
-arcpy.env.outputCoordinateSystem = arcpy.SpatialReference(4326)
+    # Set the spatial reference of the data (WGS 1984)
+    arcpy.env.outputCoordinateSystem = arcpy.SpatialReference(4326)
 
-# Create a fishnet shp file for the two different files
-outFeatureClass = "species" + "fishnet.shp"
+    # Create a fishnet shp file for the two different files
+    outFeatureClass = "species" + "fishnet.shp"
 
-# Set the origin of the fishnet
-# Create a variable to change the cell size to make it dynamic (this can be changed based on user need)
-if XMax < 0:
-    XMax_Val = XMax * -1
-else:
-    XMax_Val = XMax
-if XMin < 0:
-    XMin_Val = XMin * -1
-else:
-    XMin_Val = XMin
+    # Set the origin of the fishnet
+    # Create a variable to change the cell size to make it dynamic (this can be changed based on user need)
+    if XMax < 0:
+        XMax_Val = XMax * -1
+    else:
+        XMax_Val = XMax
+    if XMin < 0:
+        XMin_Val = XMin * -1
+    else:
+        XMin_Val = XMin
 
-originCoordinate = str(XMin) + " " + str(YMin)
-yAxisCoordinate = str(XMin) + " " + str(YMin + 1)
-cellSizeWidth = (XMax_Val + XMin_Val)/50
-print(cellSizeWidth)
-cellSizeHeight = (XMax_Val + XMin_Val)/50
-numRows = ""
-numColumns = ""
-oppositeCorner = str(XMax) + " " + str(YMax)
-labels = "NO_LABELS"
-templateExtent = "#"
-geometryType = "POLYGON"
+    originCoordinate = str(XMin) + " " + str(YMin)
+    yAxisCoordinate = str(XMin) + " " + str(YMin + 1)
+    cellSizeWidth = (XMax_Val + XMin_Val)/50
+    print(cellSizeWidth)
+    cellSizeHeight = (XMax_Val + XMin_Val)/50
+    numRows = ""
+    numColumns = ""
+    oppositeCorner = str(XMax) + " " + str(YMax)
+    labels = "NO_LABELS"
+    templateExtent = "#"
+    geometryType = "POLYGON"
 
-arcpy.CreateFishnet_management(outFeatureClass, originCoordinate, yAxisCoordinate,
-                               cellSizeWidth, cellSizeHeight, numRows, numColumns,
-                               oppositeCorner, labels, templateExtent, geometryType)
+    arcpy.CreateFishnet_management(outFeatureClass, originCoordinate, yAxisCoordinate,
+                                   cellSizeWidth, cellSizeHeight, numRows, numColumns,
+                                   oppositeCorner, labels, templateExtent, geometryType)
 
-if arcpy.Exists(outFeatureClass):
-    print("Created Fishnet file successfully!")
+    if arcpy.Exists(outFeatureClass):
+        print("Created Fishnet file successfully!")
 
 
-# Join the two species using spatial join to create the heatmap
-# Heatmap will be the last output
-target_features = outFeatureClass
-join_features = saved_Layer
-out_feature_class = "species" + "heatmap.shp"
-join_operation = "JOIN_ONE_TO_ONE"
-join_type = "KEEP_ALL"
-field_mapping = ""
-match_option = "INTERSECT"
-search_radius = ""
-distance_field_name = ""
+    # Join the two species using spatial join to create the heatmap
+    # Heatmap will be the last output
+    target_features = outFeatureClass
+    join_features = saved_Layer
+    out_feature_class = "species" + "heatmap.shp"
+    join_operation = "JOIN_ONE_TO_ONE"
+    join_type = "KEEP_ALL"
+    field_mapping = ""
+    match_option = "INTERSECT"
+    search_radius = ""
+    distance_field_name = ""
 
-arcpy.SpatialJoin_analysis(target_features, join_features, out_feature_class,
-                           join_operation, join_type, field_mapping, match_option,
-                           search_radius, distance_field_name)
+    arcpy.SpatialJoin_analysis(target_features, join_features, out_feature_class,
+                               join_operation, join_type, field_mapping, match_option,
+                               search_radius, distance_field_name)
 
-# Delete the files other than the Heatmap so that the output is cleaner
-if arcpy.Exists(out_feature_class):
-    print("Created Heatmap file successfully!")
-    print("Deleting intermediate files")
-    arcpy.Delete_management(target_features)
-    arcpy.Delete_management(join_features)
+    # Delete the files other than the Heatmap so that the output is cleaner
+    if arcpy.Exists(out_feature_class):
+        print("Created Heatmap file successfully!")
+        print("Deleting intermediate files")
+        arcpy.Delete_management(target_features)
+        arcpy.Delete_management(join_features)
 arcpy.Delete_management(os.path.join(input_directory, "temporary_files"))
